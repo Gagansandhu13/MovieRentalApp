9a19b53ebd0dff701b696b4b35f694ae0515a981
Gagandeep Sandhu <sandhugagan400@gmail.com>
2019-09-03 14:33:34 +1200

    cmt

---
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8afa6cb9-5c26-4eee-86d7-2af405d03ee5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieRentalApp;
+using System.Configuration;
+
+
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        database myData = new database();
+        [TestMethod]
+        public void TestConnection()
+        {
+            var actualDataString = myData.connectionString;
+            var expectedDataString = @"Data Source=LAPTOP-RSH35OIC\sqlexpress;Initial Catalog=MovieRental;Integrated Security=True";
+
+            Assert.AreEqual(expectedDataString, actualDataString);
+        }
+
+
+        [TestMethod]
+        public void AddCustomerTest()
+        {
+            Form1 myForm = new Form1();
+            string name = "Gagan";
+            string last = "singh";
+            string address = "123 ABC";
+            string phone = "123456789";
+            var actualdata = myForm.addCust(name, last, address, phone);
+            var expecteddata = "Data has been not Inserted";
+            Assert.AreNotEqual(expecteddata, actualdata);
+        }
+
+
+
+    }
+}
